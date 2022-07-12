973d1643f91dbb0785f8c29d612a482f1834bb10
Felipe Jaramillo <Artimax85@gmail.com>
2022-07-12 16:08:01 -0500

    Parte visual lista

---
+++ styles/felipe.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    darkMode: 'class',
+      theme: {
+        extend: {
+          fontFamily:{
+            "oswaldo": "font-family: 'Oswald', sans-serif;"
+        },
+          colors: {
+            
+          }
+        }
+      }
+  }
